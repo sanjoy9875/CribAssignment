f6db86c01912bfb4dd0580139d23329f113449cf
sanjoy <paulsanjoy9875@gmail.com>
2021-09-08 09:01:30 +0530

    showing messages in a listView with message count

--- app/src/main/java/com/example/cribassignment/MainActivity.kt
+++ app/src/main/java/com/example/cribassignment/MainActivity.kt
@@ -13,6 +13,9 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
@@ -27,6 +30,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var mEtNumber: EditText
     lateinit var mEtDays: EditText
     lateinit var mBtnFetchData: Button
+    lateinit var mProgressBar: ProgressBar
+    lateinit var listView : ListView
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +42,8 @@ class MainActivity : AppCompatActivity() {
         mEtNumber = findViewById<EditText>(R.id.etNumber)
         mEtDays = findViewById<EditText>(R.id.etDays)
         mBtnFetchData = findViewById<Button>(R.id.btnFetchData)
+        mProgressBar = findViewById<ProgressBar>(R.id.progressBar)
+        listView = findViewById<ListView>(R.id.listView)
 
         /**
          * checking Read SMS permission is granted or not
@@ -54,43 +61,75 @@ class MainActivity : AppCompatActivity() {
          */
         mBtnFetchData.setOnClickListener {
 
-            list.clear()
-            val ph = mEtNumber.text.toString()
-            val d = mEtDays.text.toString()
+            mTvText.text = ""
 
-            if (ph.isEmpty() || d.isEmpty()) {
-                list.clear()
-                mTvText.text = "Sorry,no messages found"
-            }
-
-            else if (ph.isNotEmpty() && d.isNotEmpty()) {
-                val uri = Uri.parse("content://sms/inbox")
-                val cursor = contentResolver.query(uri, null, null, null, null)
-                while (cursor?.moveToNext() == true) {
-                    val number = cursor.getString(2)
-                    val message = cursor.getString(12)
-
-                    val smsDate: String =
-                        cursor.getString(cursor.getColumnIndexOrThrow(Telephony.Sms.DATE))
-                    val dateFormat = Date(java.lang.Long.valueOf(smsDate))
-                    val formatDate: String = SimpleDateFormat("dd-MM-yyyy").format(dateFormat)
-
-                    val days = filterDays(formatDate)
-                    Log.d("TAG", "days => $days")
-
-                    if (number.contains(ph) && days <= d.toLong()) {
-                        list.add("$number \n $message \n $formatDate")
-                    }
+            /**
+             * Starting a Coroutine and fetching sms
+             */
+            CoroutineScope(Dispatchers.IO).launch {
+                mProgressBar.post {
+                    mProgressBar.visibility = View.VISIBLE
                 }
-                if (list.size > 0) {
-                    mTvText.text = "${list.size.toString()} messages found"
+                list.clear()
+                val ph = mEtNumber.text.toString()
+                val d = mEtDays.text.toString()
 
-                } else {
+                if (ph.isEmpty() || d.isEmpty()) {
                     list.clear()
-                    mTvText.text = "Sorry,no messages found"
+                    mTvText.post {
+                        mTvText.text = "Sorry,no messages found"
+                    }
+                    mProgressBar.post {
+                        mProgressBar.visibility = View.GONE
+                    }
+
+                } else if (ph.isNotEmpty() && d.isNotEmpty()) {
+                    val uri = Uri.parse("content://sms/inbox")
+                    val cursor = contentResolver.query(uri, null, null, null, null)
+                    while (cursor?.moveToNext() == true) {
+                        val number = cursor.getString(2)
+                        val message = cursor.getString(12)
+
+                        val smsDate: String =
+                            cursor.getString(cursor.getColumnIndexOrThrow(Telephony.Sms.DATE))
+                        val dateFormat = Date(java.lang.Long.valueOf(smsDate))
+                        val formatDate: String = SimpleDateFormat("dd-MM-yyyy").format(dateFormat)
+
+                        val days = filterDays(formatDate)
+                        Log.d("TAG", "days => $days")
+
+                        if (number.contains(ph) && days <= d.toLong()) {
+                            list.add("$number \n $message \n $formatDate")
+                        }
+                    }
+                    if (list.size > 0) {
+                        mTvText.post {
+                            mTvText.text = "${list.size.toString()} messages found"
+                        }
+                        mProgressBar.post {
+                            mProgressBar.visibility = View.GONE
+                        }
+
+
+                    } else {
+                        list.clear()
+                        mTvText.post {
+                            mTvText.text = "Sorry,no messages found"
+                        }
+                        mProgressBar.post {
+                            mProgressBar.visibility = View.GONE
+                        }
+                    }
                 }
             }
         }
+
+        /**
+         * Setting the list in our ListView
+         */
+        val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,list)
+        listView.adapter = adapter
+
     }
 
 
